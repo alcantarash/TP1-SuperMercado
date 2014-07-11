a5006f507a8b627291c9dab48ce94cd8474858bf
Augustosds <gu-ss@hotmail.com>
2014-07-11 18:54:13 -0300

    Update Principal.java

--- TP1/SuperMercado/src/Model/Principal.java
+++ TP1/SuperMercado/src/Model/Principal.java
@@ -1,9 +1,28 @@
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Scanner;
 
 
 public class Principal {
-
+	
+	public static void ordena(ArrayList<Produto> list){
+		Produto aux;
+		
+		for(int i = 0; i<list.size() ; i++){
+			for(int j = i ; j<list.size();j++){
+				if(list.get(j).getPreco() < list.get(i).getPreco()){
+					aux = list.get(i);
+					list.add(i, list.get(j));
+					list.add(j,aux);
+				}
+					
+			}
+		}
+		
+		
+	}
+	
+	
 	public static void main(String[] args) {
 		
 		Scanner scan = new Scanner(System.in);
@@ -29,25 +48,50 @@ public class Principal {
 					 String descricao;
 					 String validade;
 					 double preco;
+					 int estoque;				
 					 
 					 System.out.println("Digite o nome do produto: ");
 					 nome = scan.next();
 					 System.out.println("Digite a descrição do produto: ");
 					 descricao = scan.next();
 					 System.out.println("Digite a validade do produto: ");
-					 validade = scan.next();
-					 System.out.println("");					 
-					 System.out.println("Digite preço do produto: ");
+					 validade = scan.nextLine();
+					 validade = scan.nextLine();					 					 
+					 System.out.println("Digite o preço do produto: ");
 					 preco = scan.nextDouble();
+					 System.out.println("Digite o estoque do produto: ");
+					 estoque = scan.nextInt();
 					 
-					 produtos.add(item = new Produto(nome,descricao, validade, preco));					 
-					 System.out.println("Produto cadastrado com sucesso"); 
+					 produtos.add(item = new Produto(nome,descricao, validade, preco,estoque));					 
+					 System.out.println("Produto cadastrado com sucesso! \n"); break;
 					 
-					 /* Só fiz pra testar a variável static que gera os códigos na classe produto
-					 for(Produto a : produtos)
-					 System.out.println(a.getCodProd()); break;*/ 
-					 
-			
+			case 2: 
+				
+					System.out.println("\n--------Escolha uma opção--------");
+					System.out.println("1 - Listar produtos a partir dos mais baratos");
+					System.out.println("2 - Listar produtos a partir dos mais caros ");
+					System.out.println("3 - Listar produtos com maior estoque");
+					System.out.println("4 - Listar produtos mais vendidos");
+					System.out.println("5 - Listar preço médio de todos os produtos");
+					System.out.println("6 - Voltar a tela anterior");
+					
+					int opc;
+					
+					opc = scan.nextInt();
+					switch(opc){
+					
+					case 1 :// ordena(produtos);
+					
+							System.out.println(produtos.get(0).getPreco());
+							System.out.println(produtos.get(1).getPreco());
+							System.out.println(produtos.get(2).getPreco());
+							 /*for(int i = 0;i<produtos.size();i++)
+								 System.out.println(produtos.get(i).getNome()+"\n"+produtos.get(i).getDescricao()+"\n"+produtos.get(i).getPreco());*/
+							 
+								 
+						
+					}
+						 
 			}
 			
 			
